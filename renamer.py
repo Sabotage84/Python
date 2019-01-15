9a7f9a005c2c8363c57934a6fcfa8eb6242f8908
Sabotage84 <ivan.ptime@gmail.com>
2019-01-15 15:19:04 +0300

    try11

--- renamer.py
+++ renamer.py
@@ -1,12 +1,19 @@
 import os
 import random
 
+
+
+
 workdir = os.getcwd()
 files = os.listdir(workdir)
 files.remove("renamer.py")
 files_count = len(files)
 new_names = list(range(1,files_count+1))
 random.shuffle(new_names)
+
+
+
+
 
 for x in range(files_count):
 	extention = str(files[x])
