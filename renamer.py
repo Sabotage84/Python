23a171c5134f6b20ee90631c6f3e22d5f632965c
Sabotage84 <31616052+Sabotage84@users.noreply.github.com>
2021-01-28 00:00:10 +0300

    Update renamer.py

--- renamer.py
+++ renamer.py
@@ -5,15 +5,28 @@ files = os.listdir(workdir)
 files.remove("renamer.py")
 files_count = len(files)
 new_names=[]
-if ("z" in str(files[0])):
-	for x in range(files_count):
-	    new_names.append("xx"+str(x))
+if (str(files[0])=="z0"):
+        for x in range(files_count):
+                new_names.append("xx"+str(x))
+elif (str(files[0])=="xx0"):
+        for x in range(files_count):
+                new_names.append("z"+str(x))
 else:
-	for x in range(files_count):
-	    new_names.append("z"+str(x))
+        new_names2=[]
+        for x in range(files_count):
+                new_names2.append(str(x))
+        random.shuffle(new_names2)
+        for x in range(files_count):
+                extention = str(files[x])
+        new_name = str(new_names2[x])+extention[-4:]
+        os.rename(files[x], new_name)
+        for x in range(files_count):
+                new_names.append("xx"+str(x))
+        
 
 random.shuffle(new_names)
 for x in range(files_count):
-	extention = str(files[x])
-	new_name = str(new_names[x])+extention[-4:]
-	os.rename(files[x], new_name)
+        extention = str(files[x])
+        new_name = str(new_names[x])+extention[-4:]
+        os.rename(files[x], new_name)
+
